1d0d51da123f5079c66242907be04cbb86e75426
sonplusplus <vungocson189204@gmail.com>
2025-05-06 16:03:51 +0700

    Test env

--- list_envs.py
+++ list_envs.py
@@ -1,9 +1,9 @@
 import gymnasium as gym
 import ale_py
 
-# List all registered environments
+#Check if env exist in ale_py
 all_envs = gym.envs.registry.keys()
-# Filter and display only Atari environments
+
 atari_envs = [env_id for env_id in all_envs if env_id.startswith('ale_py:ALE/')]
 print("All Atari environments:")
 for env_id in atari_envs:
