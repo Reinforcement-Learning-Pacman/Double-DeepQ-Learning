358496f2cbc23a3f4069f1f9c624c0bc1002c28e
tansang06092004@gmail.com <tansang06092004@gmail.com>
2025-05-12 00:01:57 +0700

    fixlist

--- list_envs.py
+++ list_envs.py
@@ -3,7 +3,6 @@ import gymnasium as gym
 # Check if env exist in ale_py
 all_envs = gym.envs.registry.keys()
 
-atari_envs = [env_id for env_id in all_envs if env_id.startswith('ale_py:ALE/')]
-print("All Atari environments:")
-for env_id in atari_envs:
+print("All environments:")
+for env_id in all_envs:
     print(f" - {env_id}")
